7ce3d2728c4562bf91395c4c78eb0ba8026278a9
janker0718 <690864766@qq.com>
2017-07-23 22:18:01 +0800

    高阶函数 map

--- functional_programming/higher_order_functions.py
+++ functional_programming/higher_order_functions.py
@@ -10,3 +10,21 @@ print(a)
 f = abs
 
 print(f(-10))
+
+def add(x,y,f):
+    return f(x) + f(y)
+x = -5
+y = 6
+
+print(add(x,y,abs))
+
+
+
+'''
+    map 
+'''
+
+def d(x):
+    return x*x
+r = map(d,[1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(list(r))
