5022d084b4f94676332fd172a4564721eaaa4576
janker0718 <690864766@qq.com>
2017-08-28 16:25:20 +0800

    except

--- except/err.py
+++ except/err.py
@@ -1,6 +1,7 @@
 '''
  error
 '''
+import logging
 
 def foo(s):
     return 10 / int(s)
@@ -9,6 +10,31 @@ def bar(s):
     return foo(s) * 2
 
 def main():
-    bar('0')
+
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
 
 main()
+print('END')
+
+class FooError(ValueError):
+    pass
+
+def foo2(s):
+    n = int(s)
+    if n==0:
+        raise FooError('invalid value: %s' % s)
+    return 10/n
+foo2('0')
+
+def bar():
+    try:
+        foo('0')
+    except ValueError as e:
+        print('ValueError!')
+        raise
+
+bar()
+
